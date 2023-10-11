742c33dc7fab8ccb68fbd7c581b6088c53f87a98
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-11 17:00:25 -0600

    define columns for Product model

--- models/Product.js
+++ models/Product.js
@@ -2,14 +2,40 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+// I don't think I understand what this is doing or what it's here for?
+const { Promise } = require('sequelize');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
+
+/*
+product_name: 'Plain T-Shirt',
+price: 14.99,
+stock: 14,
+category_id: 1,
+*/
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
   {
     sequelize,
