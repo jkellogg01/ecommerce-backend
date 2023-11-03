d732275aaaeb86b6175bc02f83da33e8c6d588fa
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-11-03 09:43:58 -0600

    demonstration video

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 ## Description
 
-the back-end functionality of a small ecommerce site
+the back-end functionality of a small ecommerce site.
+
+[Demonstration](https://youtu.be/t8imhyzju6A)
 
 ## Table of Contents
 
@@ -30,7 +32,7 @@ Joshua Kellogg
 
 ## [License - MIT](https://opensource.org/licenses/Apache-2.0)
 
-Copyright 2023 Joshua Kellogg 
+Copyright 2023 Joshua Kellogg
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
@@ -44,7 +44,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
   const { category_name } = req.body;
   try {
@@ -64,16 +64,21 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
   const { category_name } = req.body;
+  console.log(req.body, req.params);
   try {
-    const data = await Category.update({
-      category_name,
-      where: {
-        id: req.params.id,
+    const data = await Category.update(
+      {
+        category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
       }
-    });
+    );
     if (!data) {
       res.status(404).json({
         message: "Category not found",
@@ -82,17 +87,18 @@ router.put('/:id', async (req, res) => {
     }
     res.status(200).json(data);
   } catch (err) {
+    console.error(err);
     res.status(500).json(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
   try {
     const data = await Category.destroy({
       where: {
         id: req.params.id,
-      }
+      },
     });
     if (!data) {
       res.status(404).json({
